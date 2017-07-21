91c54018f3fd23e74d8f55532ace95bd68f649fb
joshiakshay91 <joshiaks91@gmail.com>
2017-07-20 19:15:17 -0700

    CSCI - 511 Library Search engine

--- Library_search_engine/book.h
+++ Library_search_engine/book.h
@@ -1,1 +1,30 @@
+/*
 
+FileName: book.h
+Description: Header file for book cpp has display function and search other function
+Author: Akshay Joshi
+
+*/
+
+#ifndef BOOK_H
+#define BOOK_H
+#include<vector>
+#include<string>
+#include"Media.h"
+using namespace std;
+class Book :public Media
+{
+	private:
+		string author;
+		string description;
+		string publisher;
+		string city;
+		string year;
+		string series;
+		//  string notes;
+	public:
+		Book(const string& book_CN,const string& book_T,const string& book_subj, const string& book_auth, const string& book_des,const string& book_pub,const string& book_city,const string& book_year,const string& book_series,const string& book_notes);
+		void Display()const;
+		bool SearchOther(const string& keyword)const;
+};
+#endif
