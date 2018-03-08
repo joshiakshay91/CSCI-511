4477dde92727eb88945dbd14d6088b18f4eb1739
joshiakshay91 <joshiaks91@gmail.com>
2018-03-07 19:15:25 -0800

    Stamping press update

--- Stamping press/stampbuff.h
+++ Stamping press/stampbuff.h
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+//The purpose of stampbuf is to manage the char buffer(arrays of char)
+class stampbuf : public std::streambuf{
+public:
+  stampbuf(int c, int r);
+  ~stampbuf();
+void SetRow(int);//set row
+void SetColoumn(int);//set column
+virtual int sync();
+int GetRow() const;
+virtual int overflow(int);
+private:
+  char* buffer;
+  int maxRow;
+  int maxCol;
+  int row1;
+  int coloumn;
+};
