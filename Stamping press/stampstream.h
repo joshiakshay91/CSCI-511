dd9a6a0a0ddd47b34d183ea5ec857a4b2010a1a1
joshiakshay91 <joshiaks91@gmail.com>
2018-03-27 19:15:19 -0700

    Stamping press update

--- Stamping press/stampstream.h
+++ Stamping press/stampstream.h
@@ -1,1 +1,20 @@
+#include <iostream>
+class row
+{
+public:
+    row(int r);
+    int GetmyRow() const;
+private:
+    int myRow;
+};
 
+using namespace std;
+class stampstream:public ostream{
+public:
+    stampstream(int col, int row);
+    ~stampstream();
+};
+ostream& operator<<(ostream& os, const row& row);
+ostream& endrow(ostream& os);
+template< class CharT, class Traits >
+std::basic_ostream<CharT, Traits>& flush( std::basic_ostream<CharT, Traits>& os );
