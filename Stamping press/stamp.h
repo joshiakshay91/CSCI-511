06edef5b4b759962d44ccdb6037cceac5274a854
joshiakshay91 <joshiaks91@gmail.com>
2018-04-11 19:15:19 -0700

    Update for file in script

--- Stamping press/stamp.h
+++ Stamping press/stamp.h
@@ -1,10 +1,1 @@
-// Version 3/2011
-// File: stamp.h
 
-namespace stamping_press {
- void set_die(char die);
- char get_die();
- void insert_plate(int cols, int rows);
- void eject_plate();
- void stamp(int col, int row);
-};
